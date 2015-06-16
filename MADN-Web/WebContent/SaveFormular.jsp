fa232ac40488e6eb45a57974f13a158ad0e07455
Micho <Micho@Studium.fh-reutlingen.de>
2015-06-16 13:21:53 +0200

    save

--- MADN-Web/WebContent/SaveFormular.jsp
+++ MADN-Web/WebContent/SaveFormular.jsp
@@ -1,21 +1,22 @@
-
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Save Formular</title>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
 </head>
 <body>
-<divstyle="width:100%;" align="center"></div>
+	<h2>Speichern Sie das Spiel</h1>
 <p>
-<input name="filename" type="text" size="30" maxlength="30"><br/>
-<input type="radio" name="saveCSV" value="CSV"> CSV <br/>
-<input type="radio" name="saveSER" value="SER"> SER <br/>
-<input type="radio" name="savePDF" value="PDF"> PDF <br/>
+<form action="../SpeichernServlet" methode="post">
+	Dateiname: <input name="Dateiname" type="text" size="30" maxlength="30">
+	<select name="auswahl">
+		<option value="csv">.csv</option>
+		<option value="ser">.ser</option>
+		<option value="pdf">.pdf</option>
+		<option value="xml">.xml</option>
+	</select>
 </p>
-<button type="submit">Speichern!</button>
-<button type ="reset">Zurücksetzen!</button>
+	<button type="submit">OK!</button>
+</form>
 </body>
 </html>
---
+++ MADN-Web/src/Servlets/SpeichernServlet.java
@@ -0,0 +1,103 @@
+package Servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.JAXBException;
+
+import Speichern_Laden.DatenzugriffCSV;
+import Speichern_Laden.DatenzugriffPDF;
+import Speichern_Laden.DatenzugriffPDFServer;
+import Speichern_Laden.DatenzugriffSerialisiert;
+import Speichern_Laden.DatenzugriffXML;
+import Speichern_Laden.iDatenzugriff;
+import Spiel.iBediener;
+
+/**
+ * Servlet implementation class SpeichernServlet
+ */
+@WebServlet("/SpeichernServlet")
+public class SpeichernServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SpeichernServlet() {
+        super();
+       
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
+		String dateiname = request.getParameter("Dateiname");
+		String dateiformat = request.getParameter("auswahl");
+		
+		if(dateiname != null && !dateiname.isEmpty()){
+			if(dateiname.contains(".")){
+				dateiname = dateiname.substring(0,dateiname.indexOf("."));
+			}
+			iBediener spiel = (iBediener) request.getServletContext().getAttribute("spiel");
+			if(spiel != null){
+				iDatenzugriff id = null;
+				String pfad =  request.getServletContext().getRealPath("gespeicherteDateien/") + "/";
+				if(dateiformat.equals("pdf")){
+					id = new DatenzugriffPDFServer(request); 
+					dateiname += ".pdf";
+					try {
+						id.spielSpeichern(spiel, pfad + dateiname);
+					} catch (JAXBException e) {
+						e.printStackTrace();
+					}
+					request.getSession().setAttribute("dateiname",  pfad + dateiname);
+					response.sendRedirect("SpeichernLaden_HTML/pdf.jsp");
+					return;
+				} else if(dateiformat.equals("csv")){
+					id = new DatenzugriffCSV(); 
+					dateiname += ".csv";
+					
+				} else if(dateiformat.equals("ser")){
+					id = new DatenzugriffSerialisiert(); 
+					dateiname += ".ser";
+				} else if(dateiformat.equals("xml")){
+					id = new DatenzugriffXML(); 
+					dateiname += ".xml";
+				}
+				
+				try {
+					id.spielSpeichern(spiel, pfad + dateiname);
+				} catch (JAXBException e) {
+					JSPHilfsmethoden.zeigeFehlerJSP(e.getMessage(), request, response);
+					return;
+				}
+				request.getSession().setAttribute("dateiname",  pfad + dateiname);
+				response.sendRedirect("SpeichernLaden_HTML/spielGespeichert.jsp");
+			}
+		}
+	}
+}
+
+					JSPHilfsmethoden.zeigeFehlerJSP(e.getMessage(), request, response);
+					return;
+				}
+				request.getSession().setAttribute("dateiname",  pfad + dateiname);
+				response.sendRedirect("SpeichernLaden_HTML/spielGespeichert.jsp");
+			}
+		}
+	}
